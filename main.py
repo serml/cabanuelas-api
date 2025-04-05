3d1fe5316fe8b7e2736a27ce13ea60b651ba9ae8
Sergio Muñoz <sergio.munoz@upm.es>
2025-04-06 00:32:56 +0200

    origins

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ origins = [
 # Add CORSMiddleware to allow cross-origin requests
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],  # Allows all HTTP methods
     allow_headers=["*"],  # Allows all headers
